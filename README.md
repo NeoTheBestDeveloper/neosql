1c9e792ece282e90a54bbccd50392c12613e916f
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2023-06-23 12:36:44 +0300

    build: add ddl lib into pkg.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Neosql
+# neosql
 
 Neosql it is a python sql library with C backend. Just my relation database implementation.
 
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,7 @@
 git clone --depth 1 --branch v0.0.1 https://github.com/NeoTheBestDeveloper/neosql-core
 cd neosql-core
-sh scripts/release.sh
+git pull && sh scripts/release.sh
 cd -
 mv ./neosql-core/build_release/usr/local/lib/_neosql_core.so . 
-poetry build && rm _neosql_core.so
+mv ./neosql-core/build-mingw_release/usr/local/bin/_neosql_core.dll . 
+poetry build && rm _neosql_core.so && rm _neosql_core.dll
